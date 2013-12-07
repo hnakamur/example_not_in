888b21c3df0d089144de5a0d7862dd40d93469cc
Hiroaki Nakamura <hnakamur@gmail.com>
2013-12-08 01:33:04 +0900

    Use scope instead of find_by_sql to obtain ActiveRecord::Relation.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,9 @@
 class Post < ActiveRecord::Base
   acts_as_voteable
+  scope :voted_by, lambda{|voter_id|
+    joins(:votes).where(votes: {voter_id: voter_id})
+  }
+  scope :not_voted_by, lambda{|voter_id|
+    where("NOT EXISTS (SELECT * FROM votes WHERE votes.voteable_id = posts.id AND votes.voteable_type = ? AND votes.voter_id = ?)", 'Post', voter_id)
+  }
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -14,26 +14,13 @@ describe Post do
     let(:user) { FactoryGirl.create(:user) }
     let(:another_user) { FactoryGirl.create(:user) }
 
-    def voted_by(user_id)
-      Post.joins(:votes).where(votes: {voter_id: user_id})
-    end
-
-    def not_voted_by(user_id)
-      Post.find_by_sql([%Q{
-        select posts.* from posts where not exists (
-          select null from votes where voteable_id = posts.id and
-          voteable_type = 'Post' and voter_id = ?
-        )
-      }, user_id])
-    end
-
     it { expect(Post.count).to eq(15) }
 
     it "no voted posts" do
-      expect(voted_by(user.id).count).to eq(0)
+      expect(Post.voted_by(user.id).count).to eq(0)
     end
     it "15 not_voted posts" do
-      expect(not_voted_by(user.id).count).to eq(15)
+      expect(Post.not_voted_by(user.id).count).to eq(15)
     end
 
     context "with posts voted by another_user" do
@@ -44,18 +31,18 @@ describe Post do
       end
       context "user" do
         it "no voted posts" do
-          expect(voted_by(user.id).count).to eq(0)
+          expect(Post.voted_by(user.id).count).to eq(0)
         end
         it "15 not_voted posts" do
-          expect(not_voted_by(user.id).count).to eq(15)
+          expect(Post.not_voted_by(user.id).count).to eq(15)
         end
       end
       context "another_user" do
         it "15 voted posts" do
-          expect(voted_by(another_user.id).count).to eq(15)
+          expect(Post.voted_by(another_user.id).count).to eq(15)
         end
         it "no not_voted posts" do
-          expect(not_voted_by(another_user.id).count).to eq(0)
+          expect(Post.not_voted_by(another_user.id).count).to eq(0)
         end
       end
     end
@@ -69,18 +56,18 @@ describe Post do
       end
       context "user" do
         it "15 voted posts" do
-          expect(voted_by(user.id).count).to eq(15)
+          expect(Post.voted_by(user.id).count).to eq(15)
         end
         it "0 not_voted posts" do
-          expect(not_voted_by(user.id).count).to eq(0)
+          expect(Post.not_voted_by(user.id).count).to eq(0)
         end
       end
       context "another_user" do
         it "15 voted posts" do
-          expect(voted_by(another_user.id).count).to eq(15)
+          expect(Post.voted_by(another_user.id).count).to eq(15)
         end
         it "no not_voted posts" do
-          expect(not_voted_by(another_user.id).count).to eq(0)
+          expect(Post.not_voted_by(another_user.id).count).to eq(0)
         end
       end
     end
